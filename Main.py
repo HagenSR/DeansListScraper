528737c6ff3a33727d21d74136624175745804b7
Sean Hagen <sean.r.hagen@ndus.edu>
2020-01-30 15:38:18 -0600

    Cleaned up output and code. TODO work on 'other states and countries' lists

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ import Utility
 if __name__ == "__main__":
     # NDSU likes to hide the deans lists, so you need to manually find the landing page for each semester
     listLandingPage = ['https://www.ndsu.edu/news/studentnews/deanslistspring2019/'
-        , 'https://www.ndsu.edu/news/studentnews/deanslistfall2018/'
+        ,'https://www.ndsu.edu/news/studentnews/deanslistfall2018/'
         , 'https://www.ndsu.edu/news/studentnews/deanslistspring2018/'
         , 'https://www.ndsu.edu/news/studentnews/deanslistfall2017/'
         , 'https://www.ndsu.edu/news/studentnews/deanslistspring2017/'
@@ -20,49 +20,45 @@ if __name__ == "__main__":
         , 'https://www.ndsu.edu/news/studentnews/deanslistfall2012/'
         , 'https://www.ndsu.edu/news/studentnews/deanslistspring2012/']
     # opens the file to write to
-    f = open("fall2015.csv", "w")
+    f = open("allYears2012-2019.csv", "w")
     # reduced list due to large csv file
-    for link in listLandingPage[10:11]:
-        try:
-            # creates a BS object for each Semester's landing page
-            url = urllib.request.urlopen(link)
-            soup = bs.BeautifulSoup(url, 'html.parser')
-            # Find all links on the page
-            links = soup.find_all('a')
-            for x in links:
-                try:
-                    # collect information from the title of each link
-                    words = x.text.split(" ")
-                    state = ""
-                    year = words[-2] + " " + words[-1]
-                    # collect what state each link is for, or throw it away if it is not a dean's list link
-                    while not words[0].startswith("Dean"):
-                        state += " " + words[0]
-                        words.pop(0)
-                    # build a link for each state, then navigate and create a bs object for it
-                    textString = "https://www.ndsu.edu/" + x.get('href')
-                    url2 = urllib.request.urlopen(textString)
-                    soup = bs.BeautifulSoup(url2, 'html.parser')
-                    table = soup.find('table')
-                    tableRows = table.find_all('tr')
+    for link in listLandingPage:
+        # creates a BS object for each Semester's landing page
+        url = urllib.request.urlopen(link)
+        soup = bs.BeautifulSoup(url, 'html.parser')
+        # Find all links on the page
+        links = soup.find_all('a')
+        for x in links:
+            try:
+                # collect information from the title of each link
+                words = x.text.split(" ")
+                state = ""
+                year = words[-2] + " " + words[-1]
+                # collect what state each link is for, or throw it away if it is not a dean's list link
+                while "Dean" not in words[0]:
+                    words.pop(0)
+                # build a link for each state, then navigate and create a bs object for it
+                textString = "https://www.ndsu.edu/" + x.get('href')
+                url2 = urllib.request.urlopen(textString)
+                soup = bs.BeautifulSoup(url2, 'html.parser')
+                table = soup.find('table')
+                tableRows = table.find_all('tr')
 
-                    if "other" in state.lower():
-                        print(end="")
-                        # TO do, figure out how to collect information from "other" states/countries
-                        # for tr in tableRows[1:]:
-                        #     td = tr.find_all('td')
-                        #     if tr.find('td').text != "":
-                        #         location = tr.find('td').text.split(",")[0]
-                        #     row = [i.text for i in td[1:]]
-                        #     row.append(location)
-                        #     row.append(state)
-                        #     row.append(year)
-                        #     print(row)
-                    else:
-                        # collect information from each row in the table
-                        Utility.basicState(tableRows, year, f)
-                except IndexError:
+                if "other" in state.lower():
                     print(end="")
-                    # do nothing, throw away bad link
-        except Exception:
-            print("problem with url " + link)
+                    # TO do, figure out how to collect information from "other" states/countries
+                    # for tr in tableRows[1:]:
+                    #     td = tr.find_all('td')
+                    #     if tr.find('td').text != "":
+                    #         location = tr.find('td').text.split(",")[0]
+                    #     row = [i.text for i in td[1:]]
+                    #     row.append(location)
+                    #     row.append(state)
+                    #     row.append(year)
+                    #     print(row)
+                else:
+                    # collect information from each row in the table
+                    Utility.basic_state(tableRows, year, f)
+            except IndexError:
+                print(end="")
+                # do nothing, throw away bad link
--- Utility.py
+++ Utility.py
@@ -2,38 +2,41 @@ import bs4 as bs
 import urllib.request
 
 
-def goodInput(b):
-    for i in b:
-        if i is None or i.isspace():
-            return False
+def good_input(b):
+    if b is (None or b):
+        return False
     return True
 
 
-def basicState(tableRows, year, f):
-    state = ""
-    for tr in tableRows[1:]:
+def basic_state(table_rows, year, f):
+    city_state = ""
+    for tr in table_rows:
         td = tr.find_all('td')
-        # update city location if applicable
-        if tr.find('td').text != "":
-            city = tr.find('td').text.split(",")[0]
         # add information from the row/variables to a list
-        row = [i.text for i in td[1:]]
-        row.append(city)
-        row.append(state)
+        row = []
+        if td[0].text:
+            city_state = td[0].text
+        for i in td[1:]:
+            if i.text:
+                row.append(i.text)
+        row.append(city_state)
         row.append(year)
         write = ""
-        # move information from list to string
-        if goodInput(row):
+        # move information from list to string:
+        if row.__len__() is not 4:
+            print("problem with row " + str(row))
+        else:
             for i in row:
-                write += str(i) + ","
+                if i:
+                    write += str(i) + ","
             # write to exampleOutput.csv
             f.write(write + "\n")
 
 
-def linkHandler(stateLink):
+def link_handler(state_link):
     # collect information from the title of each link
-    returnList = []
-    words = stateLink.text.split(" ")
+    return_list = []
+    words = state_link.text.split(" ")
     state = ""
     year = words[-2] + " " + words[-1]
     # collect what state each link is for, or throw it away if it is not a dean's list link
@@ -41,12 +44,12 @@ def linkHandler(stateLink):
         state += " " + words[0]
         words.pop(0)
     # build a link for each state, then navigate and create a bs object for it
-    textString = "https://www.ndsu.edu/" + stateLink.get('href')
-    url2 = urllib.request.urlopen(textString)
+    text_string = "https://www.ndsu.edu/" + state_link.get('href')
+    url2 = urllib.request.urlopen(text_string)
     soup = bs.BeautifulSoup(url2, 'html.parser')
     table = soup.find('table')
-    tableRows = table.find_all('tr')
-    returnList.append(tableRows)
-    returnList.append(year)
-    returnList.append(state)
-    return returnList
+    table_rows = table.find_all('tr')
+    return_list.append(table_rows)
+    return_list.append(year)
+    return_list.append(state)
+    return return_list
